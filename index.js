dbd22c10cb38a8a9134b31d6b78ee0c3071093d9
DmitriiSaricev <ds@insomniacdesign.com>
2022-08-31 20:15:46 +0300

    add functions to card delete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
@@ -16,13 +17,12 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
-
 
 
 const PORT = process.env.PORT || 3001
