79ef9fe87f6a6ef044c6b9fb298323f36c555c34
DmitriiSaricev <ds@insomniacdesign.com>
2022-09-11 14:38:26 +0300

    connect email send

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,6 @@
 module.exports = {
 	MONGODB_URI: 'mongodb+srv://DmitriiSaricev:m3SjDCjq96Ghli5W@cluster0.nyvxj9u.mongodb.net/course-shop',
 	SESSION_SECRET: 'secret',
-	SEND_GRID_KEY: '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',
-	EMAIL_FROM: "d.saricev@yandex.ru",
+	EMAIL_FROM: "dmitriisaricev@gmail.com",
 	BASE_URL: "http://localhost:3001"
 }
